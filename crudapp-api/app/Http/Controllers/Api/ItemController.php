eef32d859c665fa8e5248620da571d0673d98877
ayoubiee <57993670+ayoubiee@users.noreply.github.com>
2024-02-29 16:19:10 +0100

    api en controller volledig crud

--- crudapp-api/app/Http/Controllers/Api/ItemController.php
+++ crudapp-api/app/Http/Controllers/Api/ItemController.php
@@ -50,4 +50,87 @@ class ItemController extends Controller
             }
         }
     }
+
+    public function show($id)
+    {
+        $item = Item::find($id);
+        if($item){
+            return response()->json([
+                'status' => 200,
+                'items' => $item
+            ], 200);
+        }else{
+            return response()->json([
+                'status' => 404,
+                'message' => "no items found"
+            ], 404);
+        }
+    }
+
+    public function edit($id)
+    {
+        $item = Item::find($id);
+        if($item){
+            return response()->json([
+                'status' => 200,
+                'items' => $item
+            ], 200);
+        }else{
+            return response()->json([
+                'status' => 404,
+                'message' => "no items found"
+            ], 404);
+        }
+    }
+
+    public function update(Request $request, int $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:191',
+            'price' => 'required|max:191'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->messages() // Use messages() instead of message()
+            ], 422);
+        } else {
+            $item = Item::find($id);
+            $item->update([
+                'name' => $request->name,
+                'price' => $request->price
+            ]);
+
+            if ($item) {
+                return response()->json([
+                    'status' => 200,
+                    'message' => "Item updated Successfully"
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => 404,
+                    'message' => "Something went wrong"
+                ], 404);
+            }
+        }
+    }
+
+    public function destroy($id)
+    {
+        $item = Item::find($id);
+        if($item){
+            $item->delete();
+            return response()->json([
+                'status' => 200,
+                'message' => "Item Deleted Successfully"
+            ], 200);
+        }else{
+            return response()->json([
+                'status' => 404,
+                'message' => "no items found"
+            ], 404);
+        }
+    }
+
 }
--- crudapp-api/routes/api.php
+++ crudapp-api/routes/api.php
@@ -10,3 +10,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('items', [Itemcontroller::class, 'index']);
 Route::post('items', [Itemcontroller::class, 'store']);
+route::get('items/{id}',[Itemcontroller::class, 'show']);
+route::get('items/{id}/edit',[Itemcontroller::class, 'edit']);
+route::put('items/{id}/edit',[Itemcontroller::class, 'update']);
+route::delete('items/{id}/delete',[Itemcontroller::class, 'destroy']);
