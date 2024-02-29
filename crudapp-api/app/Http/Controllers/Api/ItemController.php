f2310ed9a918deaf981873b99f1893c49f51d815
ayoubiee <57993670+ayoubiee@users.noreply.github.com>
2024-02-29 15:49:46 +0100

    item model imported

--- crudapp-api/app/Http/Controllers/Api/ItemController.php
+++ crudapp-api/app/Http/Controllers/Api/ItemController.php
@@ -4,8 +4,50 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Item; // Import the Item model
 
 class ItemController extends Controller
 {
-    //
+    public function index()
+    {
+        $items = Item::all();
+
+        return response()->json([
+            'status' => 200,
+            'items' => $items
+        ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:191',
+            'price' => 'required|max:191'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->messages() // Use messages() instead of message()
+            ], 422);
+        } else {
+            $item = Item::create([
+                'name' => $request->name,
+                'price' => $request->price
+            ]);
+
+            if ($item) {
+                return response()->json([
+                    'status' => 200,
+                    'message' => "Item Created Successfully"
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => 500,
+                    'message' => "Something went wrong"
+                ], 500);
+            }
+        }
+    }
 }
--- crudapp-api/routes/api.php
+++ crudapp-api/routes/api.php
@@ -2,22 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\Api\ItemController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('items', function(){
-    return 'this is item api';
-});
+Route::get('items', [Itemcontroller::class, 'index']);
+Route::post('items', [Itemcontroller::class, 'store']);
